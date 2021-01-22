a432f7a60c64fbe403dcb3b232415bc1ba772a16
Chenwi <chenwieugene.j@gmail.com>
2021-01-22 19:33:28 +0100

    sdfahg

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let wlcm_chat= prompt("welcome to Phono beats headphone landing page. what is your name?")
-let wclcm_mssg= alert("Thanks for stoping by" + wlcm_chat + "." + " This is just one of my study projects.")
+let wclcm_mssg= alert("Thanks for stoping by " + wlcm_chat + "." + " This is just one of my study projects.")
 
 
 
